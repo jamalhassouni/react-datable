56de37e83f93cf6aa15b826e959189f7f212cc9b
Jamal Hassouni <jamalhassouni13@gmail.com>
2020-04-29 23:38:47 +0000

    refactor:Refactoring App Add Dynamic Data

--- src/App.js
+++ src/App.js
@@ -1,110 +1,76 @@
 import React, { Component } from "react";
 import "./App.css";
 import DataTable from "./Components/DataTable";
+import fetch from "isomorphic-unfetch";
 
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.onAddRow = this.onAddRow.bind(this);
-
-    let model = {
+    this.state = {
       headers: [
         //{title:"Id",accessor: "id", index: 0, dataType: "number"},
+
+        { title: "#", accessor: "id", index: 1, dataType: "number" },
         {
-          title: "Profile",
-          accessor: "profile",
-          width: "80px",
-          index: 1,
-          cell: {
-            type: "image",
-            style: {
-              width: "50px",
-            },
-          },
-        },
-        {
-          title: "Name",
-          accessor: "name",
+          title: "First Name",
+          accessor: "first_name",
           width: "300px",
           index: 2,
           dataType: "string",
         },
-        { title: "Age", accessor: "age", index: 3, dataType: "number" },
         {
-          title: "Qualification",
-          accessor: "qualification",
-          index: 4,
-          dataType: "number",
+          title: "Last Name",
+          accessor: "last_name",
+          width: "300px",
+          index: 3,
+          dataType: "string",
+        },
+        {
+          title: "username",
+          accessor: "username",
+          index: 5,
+          dataType: "string",
         },
         {
           title: "Rating",
           accessor: "rating",
-          index: 5,
+          index: 6,
           width: "200px",
-          cell: (row) => (
-            <div className="rating">
-              <div
-                style={{
-                  backgroundColor: "lightskyblue",
-                  textAlign: "center",
-                  height: "1.9em",
-                  width: (row.rating / 5) * 201 + "px",
-                  margin: "3px 0 4px 0",
-                }}
-              >
-                <a href={`/showchart/${row.id}`}>{row.rating}</a>
+          cell: (row) => {
+            return (
+              <div className="rating">
+                <div
+                  style={{
+                    backgroundColor: "lightskyblue",
+                    textAlign: "center",
+                    height: "1.9em",
+                    width: (row.doctor.avg_rate / 5) * 201 + "px",
+                    margin: "3px 0 4px 0",
+                  }}
+                >
+                  <a href={`/showchart/${row.id}`}>{row.doctor.avg_rate}</a>
+                </div>
               </div>
-            </div>
-          ),
+            );
+          },
         },
       ],
-      data: [
-        {
-          id: 1,
-          name: "a",
-          age: 29,
-          qualification: "B.Com",
-          rating: 3,
-          profile: "https://via.placeholder.com/150",
-        },
-        {
-          id: 2,
-          name: "b",
-          age: 35,
-          qualification: "B.Sc",
-          rating: 5,
-          profile: "https://via.placeholder.com/150",
-        },
-        {
-          id: 3,
-          name: "c",
-          age: 42,
-          qualification: "B.E",
-          rating: 3,
-          profile: "https://via.placeholder.com/150",
-        },
-      ],
+      data: [],
+      current_page: null,
+      last_page_url: null,
+      next_page_url: null,
+      per_page: null,
+      totalRecords: null,
+      from: null,
+      to: null,
     };
-
-    for (var i = 4; i <= 20; i++) {
-      model.data.push({
-        id: i,
-        name: "name " + i,
-        age: i + 18,
-        qualification: "Graduate",
-        rating: i % 2 ? 3 : 4,
-        profile: "https://via.placeholder.com/150",
-      });
-    }
-
-    this.state = model;
   }
 
   onUpdateTable = (field, id, value) => {
     let data = this.state.data.slice();
     let updateRow = this.state.data.find((d) => {
-      return d["id"] == id;
+      return d["id"] === id;
     });
 
     updateRow[field] = value;
@@ -132,7 +98,67 @@ class App extends Component {
     });
   }
 
+  /**
+   * search For Doctors by specility | name | country | city | gender | price | rating
+   *
+   *  @param {object} values
+   *  @param {string} url
+   *  @param {number} pageNumber
+   *  @return {array} data
+   */
+  search = async (values, url = null, pageNumber = null) => {
+    let baseApiURL = "http://localhost:8000/api";
+    try {
+      let searchURL;
+      if (url && pageNumber) {
+        searchURL = `${url}?page=${pageNumber}`;
+      } else {
+        searchURL = `${baseApiURL}/search`;
+      }
+      const response = await fetch(searchURL, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "X-Requested-With": "XMLHttpRequest",
+        },
+        body: JSON.stringify(values),
+      });
+      const data = await response.json();
+      if (data.error === false) {
+        console.log("data", data);
+
+        return data.data;
+      } else {
+        return [];
+      }
+    } catch (err) {
+      console.error("error", err);
+    }
+  };
+  async componentDidMount() {
+    let res = await this.search();
+    //console.log("res.per_page", res.per_page);
+    this.setState({
+      data: res.data,
+      current_page: res.current_page,
+      last_page_url: res.last_page_url,
+      next_page_url: res.next_page_url,
+      per_page: res.per_page,
+      totalRecords: res.total,
+      from: res.from,
+      to: res.to,
+    });
+  }
+
   render() {
+    console.log("state per_page", this.state.per_page);
+    const pagination = {
+      enabled: false,
+      pageLength: this.state.per_page,
+      type: "long", // long, short
+    };
     return (
       <div>
         <button onClick={this.onAddRow}>Add random row</button>
@@ -141,16 +167,14 @@ class App extends Component {
           title="USER PROFILES"
           keyField="id"
           edit={true}
-          pagination={{
-            enabled: true,
-            pageLength: 5,
-            type: "long", // long, short
-          }}
+          pagination={pagination}
           width="100%"
           headers={this.state.headers}
           data={this.state.data}
           noData="No records!"
           onUpdate={this.onUpdateTable}
+          currentPage={this.state.current_page}
+          //totalRecords={20}
         />
       </div>
     );
